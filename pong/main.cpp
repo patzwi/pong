65623530704a56a952d1e99c28a658ebed790f75
MontyHallTufts <Monty.Hall.P123@gmail.com>
2019-10-30 16:21:44 -0400

    Initial Commit

---
+++ pong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pong
+//
+//  Created by Patrick Z on 10/30/19.
+//  Copyright © 2019 Patrick Z. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
